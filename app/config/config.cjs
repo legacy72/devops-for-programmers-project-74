449e3597faf063b179adf8a7882160c534f8e77a
legacy72 <hoperoina2016@gmail.com>
2023-06-26 22:42:46 +0400

    Test ci

--- app/config/config.cjs
+++ app/config/config.cjs
@@ -20,11 +20,15 @@ module.exports = {
     },
   },
   test: {
-    dialect: 'postgres',
-    database: process.env.DATABASE_NAME,
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    port: process.env.DATABASE_PORT,
-    host: process.env.DATABASE_HOST,
+    dialect: 'sqlite',
+    storage: './database.test.sqlite',
   },
+  // test: {
+  //   dialect: 'postgres',
+  //   database: process.env.DATABASE_NAME,
+  //   username: process.env.DATABASE_USERNAME,
+  //   password: process.env.DATABASE_PASSWORD,
+  //   port: process.env.DATABASE_PORT,
+  //   host: process.env.DATABASE_HOST,
+  // },
 };
